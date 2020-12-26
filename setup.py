12e0e8359dce4e6768f01cfd12e305795ffc2108
Viktor Persson <vph.persson@gmail.com>
2020-12-26 18:05:41 +0100

    Add `setup.py`.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='github_email_addresses',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'httpx',
+    ]
+)
